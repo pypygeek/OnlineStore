d711a9fc872c64d0862deb00d6680be2c2f80a12
서벨롭 <pypygeek@gmail.com>
2022-11-12 19:14:14 +0900

    add secret key

--- models/mongodb.py
+++ models/mongodb.py
@@ -1,6 +1,14 @@
+import os 
 import pymongo
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+Secret = os.environ.get('secret')
+
 # MONGO_URI = "mongodb+srv://<USER NAME>:<USER PASSWORD>.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI = Secret
 MONGO_CONN = pymongo.MongoClient(MONGO_URI)
 
 def conn_mongodb():
