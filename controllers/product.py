185a7aa173818db46f363a03be5602650ae71d40
서벨롭 <pypygeek@gmail.com>
2022-11-21 03:14:08 +0900

    Add detail page

--- controllers/product.py
+++ controllers/product.py
@@ -1,5 +1,5 @@
 # 상품과 관련된 API
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for
 from .blueprint import product
 
 from models.product import Product
@@ -8,11 +8,10 @@ from datetime import datetime
 from werkzeug.utils import secure_filename
 import os
 
-
+# 상품 등록 API
 @product.route('/form')
 def form():
     return render_template('product_form.html')
-
 
 @product.route('/regist', methods=['POST'])
 def regist():
@@ -20,8 +19,14 @@ def regist():
     form_data = request.form
     thumbnail_img = request.files.get('thumbnail_img')
     detail_img = request.files.get('detail_img')
-    thumbnail_img_url = _upload_file(thumbnail_img)
-    detail_img_url = _upload_file(detail_img)
+    thumbnail_img_url = ""
+    detail_img_url = ""
+
+    if thumbnail_img:
+        thumbnail_img_url = _upload_file(thumbnail_img)
+    if detail_img:
+        detail_img_url = _upload_file(detail_img)
+
     Product.insert_one(form_data, thumbnail_img_url, detail_img_url)
 
     return "상품 등록 API 입니다."
@@ -34,11 +39,44 @@ def get_products():
 
     return render_template('products.html', products=products)
 
+# 상품 삭제 API
 @product.route('/<product_id>/delete')
 def delete(product_id):
-    # 상품 삭제
     Product.delete_one(product_id)
     return "상품이 정상적으로 삭제되었습니다."
+
+# 상품 정보 수정 페이지 API
+@product.route('/<product_id>/edit')
+def edit(product_id):
+    product = Product.find_one(product_id)
+
+    return render_template('product_edit.html', product=product)
+
+# 상품 정보 수정 API
+@product.route('/<product_id>/update', methods=['POST'])
+def update(product_id):
+    form_data = request.form
+    thumbnail_img = request.files.get('thumbnail_img')
+    detail_img = request.files.get('detail_img')
+    thumbnail_img_url = ""
+    detail_img_url = ""
+
+    if thumbnail_img:
+        thumbnail_img_url = _upload_file(thumbnail_img)
+    if detail_img:
+        detail_img_url = _upload_file(detail_img)
+    
+    Product.update_one(product_id, thumbnail_img_url, detail_img_url)
+
+    return redirect(url_for('product.get_products'))
+
+
+# 상품 상세 정보 페이지 API
+@product.route('<product_id>/detail')
+def detail(product_id):
+    product = Product.find_one(product_id)
+
+    return render_template('product.html', product=product)
 
 def _upload_file(img_file):
     timestamp = str(datetime.now().timestamp())
--- models/product.py
+++ models/product.py
@@ -28,3 +28,32 @@ class Product():
     def delete_one(id):
         db = conn_mongodb()
         db.products.delete_one({'_id':ObjectId(id)})
+
+    @staticmethod
+    def update_one(id, product, thumbnail_img_url, detail_img_url):
+        db = conn_mongodb()
+
+        new_product = {
+            'name': product['name'],
+            'price': product['price'],
+            'description': product['description'],
+            'user': 'admin',
+            'updated_at': int(datetime.now().timestamp()),
+        }
+
+        if thumbnail_img_url:
+            new_product['thumbnail_img'] = thumbnail_img_url
+        if detail_img_url:
+            new_product['detail_img'] = detail_img_url
+
+        db.products.update_one(
+            {'_id': ObjectId(id)},
+            {'$set': new_product}
+        )
+
+    @staticmethod
+    def find_one(id):
+        db = conn_mongodb()
+        product = db.products.find_one({'_id': ObjectId(id)})
+
+        return product
